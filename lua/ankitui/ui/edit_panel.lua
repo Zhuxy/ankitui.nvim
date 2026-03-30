1964e1f1a30d38ad8c4cf397c22641f4a2a1dc8b
ian.zhu <redilove@gmail.com>
2026-03-30 12:39:23 +0800

    chore: harden edit panel helper

--- lua/ankitui/ui/edit_panel.lua
+++ lua/ankitui/ui/edit_panel.lua
@@ -3,6 +3,15 @@ local M = {}
 
 ---@param opts {question:{title:string,lines:string[]}, answer:{title:string,lines:string[]}, width:number, height:number, inner_width:number, pane_height:number, footer:{lines:string[], keys:table}}
 function M.create(opts)
+  opts = opts or {}
+  local question_opts = opts.question or {}
+  local answer_opts = opts.answer or {}
+  local footer_opts = opts.footer or { lines = {}, keys = {} }
+  local question_lines = question_opts.lines or {}
+  local answer_lines = answer_opts.lines or {}
+  local footer_lines = footer_opts.lines or {}
+  local footer_keys = footer_opts.keys or {}
+
   local wrapper = Snacks.win({
     border = "rounded",
     width = opts.width,
@@ -15,10 +24,12 @@ function M.create(opts)
   })
 
   local panel = { wrapper = wrapper }
-  panel.question = vim.api.nvim_create_buf(true, false)
-  panel.answer = vim.api.nvim_create_buf(true, false)
-  vim.api.nvim_buf_set_lines(panel.question, 0, -1, false, opts.question.lines)
-  vim.api.nvim_buf_set_lines(panel.answer, 0, -1, false, opts.answer.lines)
+  panel.question = vim.api.nvim_create_buf(false, true)
+  panel.answer = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_lines(panel.question, 0, -1, false, question_lines)
+  vim.api.nvim_buf_set_lines(panel.answer, 0, -1, false, answer_lines)
+  vim.bo[panel.question].bufhidden = "wipe"
+  vim.bo[panel.answer].bufhidden = "wipe"
 
   panel.question_win = vim.api.nvim_open_win(panel.question, true, {
     relative = "win",
@@ -28,7 +39,7 @@ function M.create(opts)
     width = opts.inner_width,
     height = opts.pane_height,
     border = "rounded",
-    title = opts.question.title,
+    title = question_opts.title,
     zindex = 121,
   })
   panel.answer_win = vim.api.nvim_open_win(panel.answer, false, {
@@ -39,12 +50,12 @@ function M.create(opts)
     width = opts.inner_width,
     height = opts.pane_height,
     border = "rounded",
-    title = opts.answer.title,
+    title = answer_opts.title,
     zindex = 121,
   })
 
   panel.footer = Snacks.win({
-    text = opts.footer.lines,
+    text = footer_lines,
     border = "rounded",
     width = opts.inner_width,
     height = 3,
@@ -54,7 +65,7 @@ function M.create(opts)
     col = 2,
     focusable = false,
     zindex = 121,
-    keys = opts.footer.keys,
+    keys = footer_keys,
   })
 
   function panel:set_footer(lines)
@@ -66,7 +77,17 @@ function M.create(opts)
   end
 
   function panel:close()
-    wrapper:close()
+    if self.question_win and vim.api.nvim_win_is_valid(self.question_win) then
+      vim.api.nvim_win_close(self.question_win, true)
+    end
+    if self.answer_win and vim.api.nvim_win_is_valid(self.answer_win) then
+      vim.api.nvim_win_close(self.answer_win, true)
+    end
+    if wrapper and wrapper.close then
+      wrapper:close()
+    else
+      Snacks.safe_close(wrapper)
+    end
     Snacks.safe_close(self.footer)
     if vim.api.nvim_buf_is_valid(self.question) then
       vim.api.nvim_buf_delete(self.question, { force = true })
